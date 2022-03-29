9364a4171c3bf10103355f5b9c6132435964a793
ryuvi <vicenters10@gmail.com>
2022-03-29 15:40:03 -0300

    v1.2

--- install_tools.sh
+++ install_tools.sh
@@ -27,6 +27,22 @@ install_pckg() {
     fi
 }
 
+finish_oh-my-zsh() {
+    # INSTALL OH-MY-ZSH
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    # INSTALL ZINIT
+    sh -c "$(curl -fsSL https://git.io/zinit-install)"
+    
+    FAST_SYNTAX="zinit light zdharma-continuum/fast-syntax-highlighting"
+    ZSH_AUTOSUGGESTIONS="zinit light zsh-users/zsh-autosuggestions"
+    ZSH_COMPLETIONS="zinit light zsh-users/zsh-completions"
+    
+    echo $FAST_SYNTAX >> $HOME/.zshrc
+    echo $ZSH_AUTOSUGGESTIONS >> $HOME/.zshrc
+    echo $ZSH_COMPLETIONS >> $HOME/.zshrc
+}
+
 # ----------------------- #
 #    INSTALL PACKAGES     #
 # ----------------------- #
