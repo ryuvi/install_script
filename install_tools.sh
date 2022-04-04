59ae657dfbec6b6af9922f204e466360fabbcc0d
ryuvi <vicenters10@gmail.com>
2022-04-04 18:53:30 -0300

    Added more apps

--- install_tools.sh
+++ install_tools.sh
@@ -34,16 +34,19 @@ finish_oh-my-zsh() {
     # INSTALL ZINIT
     sh -c "$(curl -fsSL https://git.io/zinit-install)"
 
+    # SET SOME VARIABLES TO PUT ON .zshrc
     FAST_SYNTAX="zinit light zdharma-continuum/fast-syntax-highlighting"
     ZSH_AUTOSUGGESTIONS="zinit light zsh-users/zsh-autosuggestions"
     ZSH_COMPLETIONS="zinit light zsh-users/zsh-completions"
 
+    # PUT THE VARIABLE ON .zshrc
     echo $FAST_SYNTAX >> $HOME/.zshrc
     echo $ZSH_AUTOSUGGESTIONS >> $HOME/.zshrc
     echo $ZSH_COMPLETIONS >> $HOME/.zshrc
 }
 
 config_vim() {
+    # INSTALL ULTIMATE VIM
     git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
     sh ~/.vim_runtime/install_awesome_vimrc.sh
 }
