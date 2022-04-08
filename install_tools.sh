fe9b0af86b094541ec14ae32de22c923e33c9d52
ryuvi <vicenters10@gmail.com>
2022-04-08 14:16:28 -0300

    made some changes

--- install_tools.sh
+++ install_tools.sh
@@ -1,118 +1,177 @@
 #!/bin/bash
 
 # ----------------------- #
-#        FUNCTIONS        #
+#                         #
+#        OS SETTER        #
+#                         #
 # ----------------------- #
-install_pckg() {
-    OS=$1
-    APP=$2
+OS=`cat /etc/os-release | grep ID_LIKE | grep -o '[a-z]' | tr -d '\n'`
 
-    echo "#####-#####-#####-#####-#####"
-    echo "#####-#####-#####-#####-#####"
-    echo "#####                   #####"
-    echo "#####       $APP        #####"
-    echo "#####                   #####"
-    echo "#####-#####-#####-#####-#####"
-    echo "#####-#####-#####-#####-#####"
+if [[ $OS == 'arch' ]]; then
+    INSTALLER=sudo pacman -S --noconfirm
+else
+    INSTALLER=sudo apt install -y
+fi
 
-    if [[ $OS == 'arch' ]]
-    then
 
-        sudo pacman -S --noconfirm $APP
+# ---------------------------------- #
+#                                    #
+#        STANDARD ENVIRONMENT        #
+#                                    #
+# ---------------------------------- #
+$INSTALLER curl \
+    wget \
+    git \
+    ttf-fira-code \
 
-    else
 
-        sudo apt install -y $APP
 
-    fi
-}
+# ----------------------------- #
+#                               #
+#        ZSH ENVIRONMENT        #
+#                               #
+# ----------------------------- #
+$INSTALLER zsh
 
-finish_oh-my-zsh() {
-    # INSTALL OH-MY-ZSH
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://git.io/zinit-install)"
 
-    # INSTALL ZINIT
-    sh -c "$(curl -fsSL https://git.io/zinit-install)"
+echo "Inserting the zinit configuration on .zshrc ..."
+echo "zinit light zdharma-continuum/fast-syntax-highlighting" >> $HOME/.zshrc
+echo "zinit light zsh-users/zsh-autosuggestions" >> $HOME/.zshrc
+echo "zinit light zsh-users/zsh-completions" >> $HOME/.zshrc
 
-    # SET SOME VARIABLES TO PUT ON .zshrc
-    FAST_SYNTAX="zinit light zdharma-continuum/fast-syntax-highlighting"
-    ZSH_AUTOSUGGESTIONS="zinit light zsh-users/zsh-autosuggestions"
-    ZSH_COMPLETIONS="zinit light zsh-users/zsh-completions"
 
-    # PUT THE VARIABLE ON .zshrc
-    echo $FAST_SYNTAX >> $HOME/.zshrc
-    echo $ZSH_AUTOSUGGESTIONS >> $HOME/.zshrc
-    echo $ZSH_COMPLETIONS >> $HOME/.zshrc
-}
 
-config_vim() {
-    # INSTALL ULTIMATE VIM
-    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh
-}
+# --------------------------------------- #
+#                                         #
+#        TEXT EDITOR CONFIGURATION        #
+#                                         #
+# --------------------------------------- #
+echo -e "Which text editor you want to use?\n [1] vim\n [2] neovim\n [3] emacs"
+read TEXT_EDITOR_ANSWER
 
-install_hyper() {
-    wget -c --content-disposition https://releases.hyper.is/download/AppImage
+case $TEXT_EDITOR_ANSWER in
 
-    mv Hyper*.AppImage $HOME/Applications
-}
+    1 || "vim")
+        $INSTALLER vim
+        git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+        sh ~/.vim_runtime/install_awesome_vimrc.sh
+        ;;
 
-install_term() {
-    echo -e "Which terminal emulator you want to use?\n [1] alacritty\n [2] tilix\n [3] xterm\n [4] hyper"
-    read ANSWER
+    2 || "neovim") $INSTALLER neovim ;;
+    3 || "emacs") $INSTALLER emacs ;;
 
-    case $ANSWER in
-        1 || "alacritty") install_pckg $OSID alacritty ;;
-        2 || "tilix") install_pckg $OSID tilix ;;
-        3 || "xterm") install_pckg $OSID xterm ;;
-        4 || "hyper") install_hyper ;;
-    esac
-}
+esac
 
-install_dock() {
-    echo -e "Which dock app you want to use?\n [1] plank\n [2] latte-dock"
-    read ANSWER
+$INSTALLER code
 
-    case $ANSWER in
-        1 || "plank") install_pckg $OSID plank ;;
-        2 || "latte-dock") install_pckg $OSID latte-dock ;;
-    esac
-}
 
-install_texteditor() {
-    echo -e "Which text editor you want to use?\n [1] vim\n [2] neovim\n [3] emacs"
-    read $ANSWER
 
-    case $ANSWER in
-        1 || "vim") install_pckg $OSID vim ;;
-        2 || "neovim") install_pckg $OSID neovim ;;
-        3 || "emacs") install_pckg $OSID emacs ;;
-    esac
-}
+# -------------------------------- #
+#                                  #
+#        TERM CONFIGURATION        #
+#                                  #
+# -------------------------------- #
+echo -e "Which terminal emulator you want to use?\n [1] alacritty\n [2] tilix\n [3] xterm\n [4] hyper"
+read TERM_ANSWER
 
-# ----------------------- #
-#    INSTALL PACKAGES     #
-# ----------------------- #
-OSID=`cat /etc/os-release | grep ID_LIKE | grep -o '[a-z]' | tr -d '\n'`
+case $TERM_ANSWER in
 
-while read LINE; do
-    INST_APP=`which $LINE`
-    
-    if [[ -n $INST_APP ]]
-    then
-        install_pckg $OSID $LINE
-    fi
+    1 || "alacritty") $INSTALLER alacritty ;;
+    2 || "tilix") $INSTALLER tilix ;;
+    3 || "xterm") $INSTALLER xterm ;;
+    4 || "hyper")
+        wget -c --content-disposition https://releases.hyper.is/download/AppImage
+        mv Hyper*.AppImage $HOME/Applications
+    ;;
 
-done < app_list;
+esac
+
+
+
+# -------------------------------- #
+#                                  #
+#        DOCK CONFIGURATION        #
+#                                  #
+# -------------------------------- #
+echo -e "Which dock app you want to use?\n [1] plank\n [2] latte-dock"
+read DOCK_ANSWER
+
+case $DOCK_ANSWER in
+
+    1 || "plank") $INSTALLER plank ;;
+    2 || "latte-dock") $INSTALLER latte-dock ;;
+
+esac
+
+
+
+# -------------------------------- #
+#                                  #
+#        JAVA CONFIGURATION        #
+#                                  #
+# -------------------------------- #
+$INSTALLER jre-openjdk-headless \
+    jre-openjdk \
+    jdk-openjdk \
+    openjdk-doc \
+    openjdk-src
+
+
+
+# -------------------------------- #
+#                                  #
+#         PHP CONFIGURATION        #
+#                                  #
+# -------------------------------- #
+$INSTALLER apache \
+    php \
+    php-apache \
+    phpmyadmin
+
+
+
+# ------------------------------------ #
+#                                      #
+#        DATABASE CONFIGURATION        #
+#                                      #
+# ------------------------------------ #
+$INSTALLER mysql sqlite3
+
+
+
+# -------------------------------------- #
+#                                        #
+#        JAVASCRIPT CONFIGURATION        #
+#                                        #
+# -------------------------------------- #
+$INSTALLER nodejs yarn
+
+
+
+# --------------------------------------- #
+#                                         #
+#        PROGRAMMING CONFIGURATION        #
+#                                         #
+# --------------------------------------- #
+$INSTALLER lua \
+    go \
+    docker \
+    ruby \
+    filezilla
 
 gem install rails
 
-install_term
-install_dock
-install_texteditor
 
-# ----------------------- #
-#     CALLING CONFIGS     #
-# ----------------------- #
-finish_oh-my-zsh
-config_vim
+
+# --------------------------------------- #
+#                                         #
+#        MISCELLANEA CONFIGURATION        #
+#                                         #
+# --------------------------------------- #
+$INSTALLER wine \
+    lutris \
+    qbittorrent \
+    peek \
+    steam
